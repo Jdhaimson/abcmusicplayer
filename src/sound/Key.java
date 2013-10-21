96a4cbd0252af68a568c335e69672062b7b039dd
= <jdhaimson@gmail.com>
2013-10-21 00:51:25 -0400

    Spec out ADT

---
+++ src/sound/Key.java
@@ -0,0 +1,35 @@
+package sound;
+
+import java.util.Map;
+
+public class Key {
+	// Stores the Key - maps notes to their proper accidentals in this key
+	private Map<String, Pitch> keySignature;
+	private String keyName;
+	
+	/**
+	 * Constructor for Key Object
+	 * @param keyName: Name of key
+	 */
+	public Key(String keyName) {
+
+	}
+	
+	/**
+	 * Alters Key with accidental on note
+	 * @param note: Note to be altered in Key signature
+	 * @param accidental: Accidental to apply to note
+	 */
+	public void alterKey(String note, String accidental) {
+		
+	}
+	
+	/**
+	 * Returns the correct pitch for the key
+	 * @param noteName: Input note name
+	 * @return Pitch with the correct accidental for the key 
+	 */
+	public Pitch getPitch(String noteName) {
+		return new Pitch('C');
+	}
+}

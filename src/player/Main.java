5a2b188e8b124e15efb3d97edb9751eb66ea2c00
afreel <austin.freel@gmail.com>
2013-10-27 23:49:32 -0400

    Open file and parse into string

--- src/player/Main.java
+++ src/player/Main.java
@@ -1,5 +1,10 @@
 package player;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Main entry point of your application.
  */
@@ -13,12 +18,34 @@ public class Main {
      * System.exit().)
      * 
      * @param file the name of input abc file
+     * @throws FileNotFoundException 
      */
-    public static void play(String file) {
-        // YOUR CODE HERE
+    public static void play(String file) throws FileNotFoundException {
+    	File song = new File(file);
+    	String content = new Scanner(song).useDelimiter("\\Z").next();
+    	String[] lines = content.split("\\r?\\n");
+    	ArrayList<String> header = new ArrayList<String>();
+    	ArrayList<String> body = new ArrayList<String>();
+    	boolean inHead = true;    	
+    	for(String line : lines){
+    		if (inHead){
+    			header.add(line);
+    		}
+    		else{
+    			body.add(line);
+    		}
+    		if (line.charAt(0) == 'K'){
+    			inHead = false;
+    		}
+    	}
+    	System.out.println(header.toString());
+    	System.out.println(body.toString());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // CALL play() HERE
+    	String file = "sample_abc/abc_song.abc";
+    	play(file);
     }
+    
 }

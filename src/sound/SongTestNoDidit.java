7de9099e2a7376d29e8f844a2e5dd84a49a8bfe6
Christopher Ford <cfordsail@gmail.com>
2013-10-30 02:01:05 -0400

    Finished Tuplet Test, revised ABCMusicHeaderParserTest

---
+++ src/sound/SongTestNoDidit.java
@@ -0,0 +1,12 @@
+package sound;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SongTestNoDidit {
+
+}
--- src/sound/TupletTest.java
+++ src/sound/TupletTest.java
@@ -147,5 +147,127 @@ public class TupletTest {
         assertEquals(true, tup1.getType() == tup2.getType());
         assertEquals(false, tup1.getType() == tup5.getType());
 	}
+	
+	@Test
+	public void testTupletGetDuration() {
+	       List<MusicalElement> notes = new LinkedList<MusicalElement>();
+	        notes.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+	        notes.add(new Note(new Pitch('G'),new Fraction(1, 4)));
+	        
+	        List<MusicalElement> notes2 = new LinkedList<MusicalElement>();
+	        notes2.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+	        notes2.add(new Note(new Pitch('F'),new Fraction(1, 4)));
+	        
+	        List<MusicalElement> notes3 = new LinkedList<MusicalElement>();
+	        notes3.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+	        notes3.add(new Note(new Pitch('G'),new Fraction(1, 2)));
+	        
+	        List<MusicalElement> notes4 = new LinkedList<MusicalElement>();
+	        notes4.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+	        notes4.add(new Note(new Pitch('G'),new Fraction(1, 4)));
+	        
+	        List<MusicalElement> notes5 = new LinkedList<MusicalElement>();
+	        notes5.add(new Note(new Pitch('C'), new Fraction(1, 8)));
+	        notes5.add(new Note(new Pitch('G'),new Fraction(1, 8)));
+	        notes5.add(new Note(new Pitch('F'), new Fraction(1, 8)));
+	        
+	        Tuplet tup1 = new Tuplet(notes);
+	        Tuplet tup2 = new Tuplet(notes2);
+	        Tuplet tup3 = new Tuplet(notes3);
+	        Tuplet tup4 = new Tuplet(notes4);
+	        Tuplet tup5 = new Tuplet(notes5);
+	        
+	        assertEquals(true, tup1.getDuration().equals(new Fraction(3, 4)));
+	        assertEquals(true, tup5.getDuration().equals(new Fraction(2, 8)));
+	        assertEquals(true, tup1.getDuration().equals(tup2.getDuration()));
+	        
+	}
+	
+	@Test
+	public void testTupletClone() {
+	   
+        List<MusicalElement> notes5 = new LinkedList<MusicalElement>();
+        notes5.add(new Note(new Pitch('C'), new Fraction(1, 8)));
+        notes5.add(new Note(new Pitch('G'),new Fraction(1, 8)));
+        notes5.add(new Note(new Pitch('F'), new Fraction(1, 8)));
+        
+        Tuplet tup5 = new Tuplet(notes5);
+        Tuplet clone = tup5.clone();
+        
+        assertEquals(true, tup5.equals(clone));
+        assertEquals(true, tup5.hashCode() == clone.hashCode());
+        assertEquals(true, tup5.toString().equals(clone.toString()));
+	}
+	
+	@Test
+	public void testTupletGetTicksPerWholeNote() {
+	    List<MusicalElement> notes = new LinkedList<MusicalElement>();
+        notes.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes.add(new Note(new Pitch('G'),new Fraction(1, 4)));
+        
+        List<MusicalElement> notes2 = new LinkedList<MusicalElement>();
+        notes2.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes2.add(new Note(new Pitch('F'),new Fraction(1, 4)));
+        
+        List<MusicalElement> notes3 = new LinkedList<MusicalElement>();
+        notes3.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes3.add(new Note(new Pitch('G'),new Fraction(1, 2)));
+        
+        List<MusicalElement> notes4 = new LinkedList<MusicalElement>();
+        notes4.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes4.add(new Note(new Pitch('G'),new Fraction(1, 4)));
+        
+        List<MusicalElement> notes5 = new LinkedList<MusicalElement>();
+        notes5.add(new Note(new Pitch('C'), new Fraction(1, 8)));
+        notes5.add(new Note(new Pitch('G'),new Fraction(1, 8)));
+        notes5.add(new Note(new Pitch('F'), new Fraction(1, 8)));
+        
+        Tuplet tup1 = new Tuplet(notes);
+        Tuplet tup2 = new Tuplet(notes2);
+        Tuplet tup5 = new Tuplet(notes5);
+    
+        assertEquals(true, tup1.getTicksPerWholeNote() == tup2.getTicksPerWholeNote());
+        assertEquals(true, tup1.getTicksPerWholeNote() == tup1.getTicksPerWholeNote());
+        assertEquals(8, tup1.getTicksPerWholeNote());
+        assertEquals(24, tup5.getTicksPerWholeNote());   
+	}
+	
+	@Test
+	public void testTupletToString() {
+	    List<MusicalElement> notes = new LinkedList<MusicalElement>();
+        notes.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes.add(new Note(new Pitch('G'),new Fraction(1, 4)));
+        
+        List<MusicalElement> notes2 = new LinkedList<MusicalElement>();
+        notes2.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes2.add(new Note(new Pitch('F'),new Fraction(1, 4)));
+        
+        List<MusicalElement> notes3 = new LinkedList<MusicalElement>();
+        notes3.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes3.add(new Note(new Pitch('G'),new Fraction(1, 2)));
+        
+        List<MusicalElement> notes4 = new LinkedList<MusicalElement>();
+        notes4.add(new Note(new Pitch('C'), new Fraction(1, 4)));
+        notes4.add(new Note(new Pitch('G'),new Fraction(1, 4)));
+        
+        List<MusicalElement> notes5 = new LinkedList<MusicalElement>();
+        notes5.add(new Note(new Pitch('C'), new Fraction(1, 8)));
+        notes5.add(new Note(new Pitch('G'),new Fraction(1, 8)));
+        notes5.add(new Note(new Pitch('F'), new Fraction(1, 8)));
+        
+        Tuplet tup1 = new Tuplet(notes);
+        Tuplet tup2 = new Tuplet(notes2);
+        Tuplet tup3 = new Tuplet(notes3);
+        Tuplet tup4 = new Tuplet(notes4);
+        Tuplet tup5 = new Tuplet(notes5);
+    
+        assertEquals(true, tup1.toString().equals(tup1.toString()));
+        assertEquals(true, tup1.toString().equals(tup4.toString()));
+        assertEquals(false, tup1.toString().equals(tup2.toString()));
+        assertEquals(false, tup1.toString().equals(tup3.toString()));
+        assertEquals(false, tup1.toString().equals(tup5.toString()));
+	}
+	
+	
 
 }

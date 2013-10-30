8ad56cb8940a259e4f72bb48c1dc5f47f6689459
afreel <austin.freel@gmail.com>
2013-10-30 03:03:41 -0400

    documented Main

--- src/player/Main.java
+++ src/player/Main.java
@@ -17,7 +17,7 @@ import sound.*;
 
 
 /**
- * Main entry point of your application.
+ * Main entry point of the application.
  */
 public class Main {
 
@@ -29,35 +29,39 @@ public class Main {
      * System.exit().)
      * 
      * @param file the name of input abc file
-     * @throws FileNotFoundException if file does not exist
+     * @throws FileNotFoundException file does not exist at specified location
      * @throws InvalidMidiDataException error with Midi player and/or sequencer
      * @throws MidiUnavailableException error with Midi player and/or sequencer
      */
     public static void play(String file) throws FileNotFoundException, MidiUnavailableException, InvalidMidiDataException {
     	File songFile = new File(file);
-    	String content = new Scanner(songFile).useDelimiter("\\Z").next();
-    	String[] lines = content.split("\\r?\\n");
+    	String content = new Scanner(songFile).useDelimiter("\\Z").next(); //reads file and extract String representation 
+    	String[] lines = content.split("\\r?\\n"); //separates content of file by individual lines
+    	
+    	//initializing header and body line lists
     	ArrayList<String> header = new ArrayList<String>();
     	ArrayList<String> body = new ArrayList<String>();
-    	boolean inHead = true;    	
+    	
+    	boolean inHead = true; //we always begin in the header   	
     	for(String line : lines){
     		if (inHead){
-    			header.add(line);
+    			header.add(line); //add header lines to header list
     		}
     		else{
-    			body.add(line);
+    			body.add(line); //add body lines to body list
     		}
+    		//key ("K") is always the last field line in header
     		if (line.charAt(0) == 'K'){
     			inHead = false;
     		}
     	}
     	
     	MusicPlayerHeader headPlayer = new MusicPlayerHeader();
-    	Song song = headPlayer.parse(join(header,"\n"));
-    	System.out.println(song);
+    	Song song = headPlayer.parse(join(header,"\n")); //join header lines using newline token
+    	System.out.println(song); //display header fields
     	
     	MusicPlayerBody bodyPlayer = new MusicPlayerBody(song);
-    	song = bodyPlayer.parse(join(body, "\n"));
+    	song = bodyPlayer.parse(join(body, "\n")); //join body lines using newline token
 
     	List<Measure> measures = song.getMeasures();
     	for (Measure measure : measures){
@@ -68,11 +72,16 @@ public class Main {
     }
 
     public static void main(String[] args) throws FileNotFoundException, MidiUnavailableException, InvalidMidiDataException {
-        // CALL play() HERE
     	String file = "sample_abc/piece2.abc";
     	play(file);
     }
     
+    /**
+     * Joins list of Strings into one String, separating each with a specified separator
+     * @param strings list of Strings to be joined
+     * @param separator String placed in between each string in String list "strings"
+     * @return String representing joined list of Strings
+     */
     public static String join(ArrayList<String> strings, String separator){
     	StringBuilder newStrings = new StringBuilder();
     	for(String line : strings){
